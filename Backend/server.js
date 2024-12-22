7d6df3df348f84308efae546704e839af75c87de
MahirAhmadEbrahimi <mahirahmadebrahimi@gmail.com>
2024-12-22 16:56:03 +0430

    add Cors MOdel and User Login and Registration is compleatd.

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ import hotel from "./routes/hotelRoute.js";
 import booking from "./routes/bookingRoute.js";
 const app = express();
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
